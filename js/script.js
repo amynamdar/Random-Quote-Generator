c3eb8d740d5c9c003d792f348ec1e2ddc26ae47f
Amy Namdar <amynamdar@users.noreply.github.com>
2022-08-05 09:03:32 -0400

    Created/tested the array of quotes

--- js/script.js
+++ js/script.js
@@ -30,4 +30,41 @@ project 1 - A Random Quote Generator
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+//document.getElementById('load-quote').addEventListener("click", printQuote, false);
+console.log("Testing: 1-2-3! The script is connected to the HTML!");
+
+// Array of quotes
+
+quotes = [
+  {
+    quote:"Double, double toil and trouble; Fire burn and caldron bubble.",
+    source:"William Shakespeare",
+    citation:" Macbeth: Song of the Witches",
+    year: "1623"
+
+  },
+  {
+    quote:"Jack and Jill went up the hill, To fetch a pail of water; Jack fell down, and broke his crown. And Jill came tumbling after.",
+    source:"John Newbery",
+    citation:"Mother Goose's Melody",
+    year: "1765"
+  },
+  {
+    quote:"Le doute est un état mental désagréable, mais la certitude est ridicule.",
+    source:"Voltaire",
+    citation:"Oeuvres Completes de Voltaire, Volume 12, Part 1",
+    year: "1817"
+  },
+  {
+    quote:"When the phone rang I was in the kitchen, boiling a potful of spaghetti and whistling along with an FM broadcast of the overture to Rossini's 'The Thieving Magpie,' which has to be the perfect music for cooking pasta.",
+    source:"Haruki Murakami",
+    citation:"The Wind-Up Bird Chronicle",
+    year: "1997"
+  },
+  {
+    quote:"Toast cannot be explained by any rational means.",
+    source:"Margaret Atwood",
+    citation:"Oryx and Crake",
+    year: "2003"
+  }
+];
